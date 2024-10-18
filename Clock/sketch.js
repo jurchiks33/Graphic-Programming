f9a5f88c059727299ffc6b2ff6521bb7ee265c66
jurchiks33 <jurisslisans@gmail.com>
2024-10-18 08:52:26 +0200

    initial clock

--- Clock/sketch.js
+++ Clock/sketch.js
@@ -1,9 +1,28 @@
-function setup() {
+var secLength = 160;
+var secWidth = 1;
+var minLenght = 140;
+var minWidth = 3;
+var hourLength = 90;
+var hourWidth = 5;
+
+
+function setup() 
+{
     createCanvas(900, 600);
     background(0);
 }
 
-function draw() {
-    background(0);
+function draw() 
+{
+    background(255);
+    translate(width/2, height/2);
+    ellipse(0, 0, 350, 350);
 
+    push();
+    strokeWeight(secWidth);
+    stroke(200, 0, 0);
+    var secAngle = map(second(), 0, 60, 0, 360);
+    rotate(radians(secAngle));
+    line(0, 0, 0, -secLength);
+    pop();
 }
