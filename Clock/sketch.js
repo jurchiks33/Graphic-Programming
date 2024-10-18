8fcbdaac0b04622a13d6c099c8f7ef8cb463a91c
jurchiks33 <jurisslisans@gmail.com>
2024-10-18 09:38:49 +0200

    little styling for a clock

--- Clock/sketch.js
+++ Clock/sketch.js
@@ -20,19 +20,23 @@ function draw()
 
     // seconds
     push();
-    strokeWeight(secWidth);
-    stroke(200, 0, 0);
-    var secAngle = map(second(), 0, 60, 0, 360);
-    rotate(radians(secAngle));
-    line(0, 0, 0, -secLength);
+        strokeWeight(secWidth);
+        stroke(200, 0, 0);
+        var secAngle = map(second(), 0, 60, 0, 360);
+        rotate(radians(secAngle));
+        line(0, 0, 0, -secLength);
     pop();
 
     //minutes
     push();
-    strokeWeight(minWidth);
-    var minAngle = map(minute(), 0, 60, 0, 360);
-    rotate(radians(minAngle));
-    line(0, 0, 0, -minLenght);
+        strokeWeight(minWidth);
+        var minAngle = map(minute(), 0, 60, 0, 360);
+        rotate(radians(minAngle));
+        line(0, 0, 0, -minLenght);
+    push();
+        translate(0, -minLenght + 20);
+        ellipse(0, 0, 15, 15);
+    pop();
     pop();
 
     //minutes
@@ -41,5 +45,21 @@ function draw()
     var hourAngle = map(hour(), 0, 12, 0, 360);
     rotate(radians(hourAngle));
     line(0, 0, 0, -hourLength);
+        push();
+            translate(0, -hourLength + 20);
+            ellipse(0, 0, 15, 15);
+        pop();
     pop();
+
+    var hours = 12;
+    var hourStep = 360 / hours;
+    for (var i=0; i<hours; i++)
+        {
+            push();
+                rotate(radians(hourStep * i));
+                translate(0, -155);
+                line(0, 0, 0, -20);
+            pop();
+        };
+
 }
