171d253466db2ad5baceabfa28d1356d65696f43
jurchiks33 <jurisslisans@gmail.com>
2024-11-05 13:22:55 +0100

    testing button

--- Object Oriented Programming/sketch.js
+++ Object Oriented Programming/sketch.js
@@ -2,13 +2,21 @@ var state = false;
 var locX = 200;
 var locY = 200;
 var w = 200;
+var h = 100;
 
 function setup() {
     createCanvas(900, 600);
-    background(0);
 }
 
 function draw() {
-    background(0);
+    background(125);
 
+    if (state==true) 
+    {
+        fill(255);
+    } else {
+        fill(0);
+    }
+
+    rect(locX, locY, w, h);
 }
