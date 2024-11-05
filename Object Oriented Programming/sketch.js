bba06580faeaa8bdcb944f5da814489c51a9c39c
jurchiks33 <jurisslisans@gmail.com>
2024-11-05 13:19:33 +0100

    files and some variables

---
+++ Object Oriented Programming/sketch.js
@@ -0,0 +1,14 @@
+var state = false;
+var locX = 200;
+var locY = 200;
+var w = 200;
+
+function setup() {
+    createCanvas(900, 600);
+    background(0);
+}
+
+function draw() {
+    background(0);
+
+}
