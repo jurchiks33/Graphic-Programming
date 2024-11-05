61a4b155d960efbc71258680d4c1b5f6e1df8365
jurchiks33 <jurisslisans@gmail.com>
2024-11-05 16:42:18 +0100

    work on a button styling

--- Object Oriented Programming/sketch.js
+++ Object Oriented Programming/sketch.js
@@ -3,8 +3,8 @@ var button2;
 
 function setup() {
     createCanvas(900, 600);
-    button = new Button(200, 200, 200, 100);
-    button2 = new Button(100, 100, 50, 50);
+    button = new Button(200, 200, 200, 100, "Button 1");
+    button2 = new Button(100, 100, 50, 50, "Button 2");
 }
 
 function draw() {
@@ -28,10 +28,21 @@ class Button
         this.locY = locY;
         this.w = w;
         this.h = h;
+        this.label = label;
+        this.glowIntensity = 9;
     }
 
     draw() 
     {
+        // shadow effect for a background
+        noStroke();
+        fill(0, 0, 0, 30);
+        rect(this.locX + 4, this.locY+ 4, this.w, this.h, 20);
+
+        // Button color with effect on it
+        let baseColor = this.state ? color(0, 150, 250) : color(100, 100, 100);
+        let glossyColor = this.state ? color(0, 200, 255, 150) : color(255, 255, 255, 80);
+
         if (this.state==true) 
             {
                 fill(255);
