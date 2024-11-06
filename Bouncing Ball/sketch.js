018da12b57cb0fc5684ad77dec312c612d6f9c6b
jurchiks33 <jurisslisans@gmail.com>
2024-11-06 14:55:47 +0100

    reworking of a ball to more styled approach

--- Bouncing Ball/sketch.js
+++ Bouncing Ball/sketch.js
@@ -1,49 +1,73 @@
-var ball;
+let balls = [];
+let stars = [];
+let comet;
 
-function setup() 
+function setup()
 {
     createCanvas(900, 600);
-    ball = new Ball;
+
+    // initialization of stars in a random position and twinkling
+    for (let i = 0; i < 200; i++)
+    {
+        stars.push(
+            {
+                x: random(width),
+                y: random(height),
+                brightness: random(50, 255),
+                speed: random(1, 4),
+                targetBrightness: random(50, 255),
+            }
+        );
+    }
 }
 
-function draw() 
-{
-    background(0);
-    ball.run();
-}
 
-class Ball 
-{
-    constructor()
-    {
-        this.velocity = new createVector(random(-2, 2), random(-2, 2));
-        this.location = new createVector(random(width), random(height));
-    }
+// var ball;
 
-    // run function for the ball
-    run() 
-    {
-        this.draw();
-        this.move();
-        this.bounce();
-    }
+// function setup() 
+// {
+//     createCanvas(900, 600);
+//     ball = new Ball;
+// }
 
-    draw()
-    {
-        fill(125);
-        ellipse(this.location.x, this.location.y, 40, 40);
-    }
+// function draw() 
+// {
+//     background(0);
+//     ball.run();
+// }
 
-    // function to move the ball
-    move()
-    {
-        this.location.add(this.velocity);
-    }
+// class Ball 
+// {
+//     constructor()
+//     {
+//         this.velocity = new createVector(random(-2, 2), random(-2, 2));
+//         this.location = new createVector(random(width), random(height));
+//     }
 
-    bounce() 
-    {
-        if (this.location.x < 0 || this.location.x > width) this.velocity.x *= -1;
-        if (this.location.y < 0 || this.location.y > height) this.velocity.y *= -1;
-    }
+//     // run function for the ball
+//     run() 
+//     {
+//         this.draw();
+//         this.move();
+//         this.bounce();
+//     }
 
-}
+//     draw()
+//     {
+//         fill(125);
+//         ellipse(this.location.x, this.location.y, 40, 40);
+//     }
+
+//     // function to move the ball
+//     move()
+//     {
+//         this.location.add(this.velocity);
+//     }
+
+//     bounce() 
+//     {
+//         if (this.location.x < 0 || this.location.x > width) this.velocity.x *= -1;
+//         if (this.location.y < 0 || this.location.y > height) this.velocity.y *= -1;
+//     }
+
+// }
