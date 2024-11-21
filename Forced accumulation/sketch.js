84bde2e33771eaddbc7db74e9eb84640f09acec7
jurchiks33 <jurisslisans@gmail.com>
2024-11-21 17:08:38 +0100

    minor adjusting to the code for a project

--- Forced accumulation/sketch.js
+++ Forced accumulation/sketch.js
@@ -16,8 +16,10 @@ class Ball
 {
     constructor()
     {
-        this.velocity = new createVector(random(-2, 2), random(-2, 2));
-        this.location = new createVector(random(width), random(height));
+        this.acceleration = new createVector(0, 0);
+        this.velocity = new createVector(0, 0);
+        this.location = new createVector(width/2, height/2);
+        this.size = 40;
     }
 
     // run function for the ball
@@ -31,12 +33,13 @@ class Ball
     draw()
     {
         fill(125);
-        ellipse(this.location.x, this.location.y, 40, 40);
+        ellipse(this.location.x, this.location.y, this.size, this.size);
     }
 
     // function to move the ball
     move()
     {
+        this.velocity.add(this.acceleration);
         this.location.add(this.velocity);
     }
 
