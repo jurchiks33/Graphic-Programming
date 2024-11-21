1633df2456801b248e1d89d7ab51932f9a5c2bf1
jurchiks33 <jurisslisans@gmail.com>
2024-11-21 12:07:45 +0100

    finishing up styling

--- Follow the Mouse/sketch.js
+++ Follow the Mouse/sketch.js
@@ -1,21 +1,20 @@
-let ball;
-let numBalls = 10;
+let balls = []; // Array to hold multiple balls
+let numBalls = 10; // Number of balls
 
 function setup() 
 {
     createCanvas(900, 600);
-    background(0);
-    for (let i = 0; i < numBalls; i++)
-    {
-        balls.push(new Ball());
+    background(0); 
+    for (let i = 0; i < numBalls; i++) {
+        balls.push(new Ball()); // Initialize each ball and push to the array
     }
 }
 
 function draw() 
 {
-    for (let ball of balls)
-    {
-        ball.run();
+    // Do not repaint the background to let the trails persist
+    for (let ball of balls) {
+        ball.run(); 
     }
 }
 
@@ -23,60 +22,54 @@ class Ball
 {
     constructor()
     {
-        // random starting position for a ball
+        // Random starting position for the ball
         let randomX = width / 2 + random(-100, 100);
         let randomY = height / 2 + random(-100, 100);
-        this.location = new createVector(randomX, randomY);
-        this.prevLocation = new createVector(randomX, randomY);
+        this.location = createVector(randomX, randomY); // Current position
+        this.prevLocation = createVector(randomX, randomY); // Previous position
 
-        // initial velocity and acceleration
-        this.velocity = new createVector(0, 0);
-        this.acceleration = new createVector(0, 0);
+        // Initial velocity and acceleration
+        this.velocity = createVector(0, 0);
+        this.acceleration = createVector(0, 0);
 
-        // maximum velocity
+        // Maximum velocity for smooth movement
         this.maxVelocity = random(5, 10);
 
-        // random color for ball trails
+        // Random color for ball trails
         this.color = color(random(50, 255), random(50, 255), random(50, 255));
     }
 
-    // run function for the ball
+    // Run function for the ball
     run() 
     {
-        this.draw();
-        this.move();
+        this.draw(); 
+        this.move(); 
     }
 
     draw()
     {
-        // draw for the trail
+        // Draw the trail
         stroke(this.color);
         strokeWeight(2);
         line(this.location.x, this.location.y, this.prevLocation.x, this.prevLocation.y);
 
-        // updating previous location
+        // Update the previous location
         this.prevLocation = this.location.copy();
     }
 
-    // function to move the ball
+    // Function to move the ball
     move()
     {
-        var mouse = createVector(mouseX, mouseY);
-        var dir = p5.Vector.sub(mouse, this.location);
+        // Attract the ball to the mouse position
+        let mouse = createVector(mouseX, mouseY);
+        let dir = p5.Vector.sub(mouse, this.location); // Direction vector
         dir.normalize();
-        dir.mult(0.3);
+        dir.mult(0.3); // Control the strength of the attraction
         this.acceleration = dir;
+
+        // Update velocity and location
         this.velocity.add(this.acceleration);
         this.velocity.limit(this.maxVelocity);
         this.location.add(this.velocity);
     }
-
-    edges() 
-    {
-        if (this.location.x < 0) this.location.x=width;
-        else if (this.location.x > width) this.location.x = 0;
-        else if (this.location.y < 0) this.location.y = height;
-        else if (this.location.y > height) this.location.y = 0;
-    }
-
 }
