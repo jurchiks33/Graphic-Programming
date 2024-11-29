5dca698a5855c536d3cb9129717677078dfe1277
jurchiks33 <jurisslisans@gmail.com>
2024-11-29 09:15:06 +0100

    creating files for a matter engine test

---
+++ Testing matter engine/sketch.js
@@ -0,0 +1,18 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine;
+var box1;
+
+function setup() {
+    createCanvas(900, 600);
+
+
+    engine = Engine.create();
+}
+
+function draw() {
+
+}
