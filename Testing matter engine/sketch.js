bffdf3e70cdcea1591a3fed070b9952194f0b5a0
jurchiks33 <jurisslisans@gmail.com>
2024-12-02 09:15:24 +0100

    remowing objects outside screen

--- Testing matter engine/sketch.js
+++ Testing matter engine/sketch.js
@@ -37,11 +37,29 @@ function draw(){
     fill(125);
     drawVertices(ground1.vertices);
     drawVertices(ground2.vertices);
+
+  generateObject(width/2, 0);
+
+    for (var i=0; i<boxes.length; i++) {
+      drawVertices(boxes[i].vertices);
+
+      if (isOffScreen(boxes[i])) {
+          World.remove(engine.world, boxes[i]);
+          boxes.splice(i, 1);
+          i--;
+      }
+    }
 }
 
-function generateObject(x, y) {
+function generateObject(x, y){
   var b = Bodies.rectangle(x, y, random(10, 30), random(10,30), {restitution:.8, friction: .5});
   boxes.push(b);
+  World.add(engine.world, [b]);
+}
+
+function isOffScreen(body){
+  var pos = body.position;
+  return (pos.y > height || pos.x < 0 || pos.x > width);
 }
 
 function drawVertices(vertices) {
