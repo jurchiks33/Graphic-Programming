ffb4fdb19f128d8d8ab36efa529dd08a1cb464a6
jurchiks33 <jurisslisans@gmail.com>
2025-02-10 15:44:50 +0100

    camera moving around

--- Camera/sketch.js
+++ Camera/sketch.js
@@ -8,10 +8,15 @@ function draw() {
 
     //var zLoc = (sin(frameCount)+1) / 2 * height + 200;
     //var zLoc = map(sin(frameCount)+1, -1, 1, 200, 800);
-    var xLoc = map(noise(frameCount / 100), 0, 1, -500, 500);
-    var yLoc = map(noise(frameCount / 100 + 100), 0, 1, -500, 500);
-    var zLoc = map(noise(frameCount / 100 + 150), 0, 1, 300, 800);
 
+
+    // var xLoc = map(noise(frameCount / 100), 0, 1, -500, 500);
+    // var yLoc = map(noise(frameCount / 100 + 100), 0, 1, -500, 500);
+    // var zLoc = map(noise(frameCount / 100 + 150), 0, 1, 300, 800);
+
+    var xLoc = cos(frameCount) * height;
+    var yLoc = sin(frameCount) * 300;
+    var zLoc = sin(frameCount) * height;
     camera(xLoc, yLoc, zLoc, 0, 0, 0, 0, 1, 0);
 
     normalMaterial();
